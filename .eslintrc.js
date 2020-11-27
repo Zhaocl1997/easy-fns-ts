59ea06dc8986e7da8b1e4651ce8db8ed7cc14ade
zhaocl1997 <674351624@qq.com>
2020-11-27 16:59:05 +0800

    chore(.eslintrs.js): add some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,12 +21,17 @@ module.exports = {
   rules: {
     "no-shadow": "off",
     "max-len": "off",
-    "@typescript-eslint/no-shadow": ["error"],
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "off",
     "no-plusplus": "off",
+    "no-bitwise": "off",
     "no-use-before-define": "off",
     "no-unused-expressions": "off",
+
+    "@typescript-eslint/no-shadow": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+
     "import/extensions": [
       "error",
       "ignorePackages",
